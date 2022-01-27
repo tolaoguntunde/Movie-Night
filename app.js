ebb80ad3f088854771bc9202e0b197264e7ab8aa
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2022-01-26 21:40:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form");
 const movieDisplay = document.querySelector(".movie-result");
 const movieSearchByUser= document.querySelector("#msearch");
+const movieBody = document.querySelector(".movie-body");
 
 
 async function getMovies(data){
@@ -13,7 +14,8 @@ async function getMovies(data){
     const rating = resData.imdbRating;
     const duration = resData.Runtime;
     console.log(title,year,rating,duration);
-    movieDisplay.innerHTML = `<h1 style= color:red >Movie- ${title}</h1>  <p> ${title} was released in year ${year} </p> <p> IMDB rating is ${rating}</p>
+    movieBody.style.display = 'none';
+    movieDisplay.innerHTML = `<h1 style= color:red >Movie- ${title}</h1>  <p> ${title} released in year ${year} </p> <p> IMDB rating is ${rating}</p>
     <p> Duration ${duration}</p><img src="${image}">`
     
 }
@@ -21,9 +23,11 @@ async function getMovies(data){
 
 form.addEventListener("submit",function(e){
     e.preventDefault();
+    
     const searchTerm =movieSearchByUser.value;
     const apiKey = "8f802cb8";
     const url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`;
+   
     getMovies(url);
 
     
